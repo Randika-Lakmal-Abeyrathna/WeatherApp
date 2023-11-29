d2376a76b6c3f6d61d9684d0fa5a859893976862
Randika Lakmal <randika.l.abeyrathna@gmail.com>
2023-11-29 08:33:34 +0530

    Complteted the skeleton view

---
+++ src/components/CityCardSkeleton.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex py-6 px-3 bg-wather-secoundary rounded-md shadow-md ">
+        <div class="flex flex-col flex-1 gap-2">
+            <AnimatedPlaceholder class="max-w-[50%]"/>
+            <AnimatedPlaceholder class="max-w-[40%]" />
+        </div>
+
+        <div class="flex flex-col items-end flex-1 gap-2">
+            <AnimatedPlaceholder class="max-w-[50%] w-full"/>
+            <AnimatedPlaceholder class="max-w-[75%] w-full" />
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+import AnimatedPlaceholder from  "./AnimatedPlaceholde.vue";
+
+
+</script>
